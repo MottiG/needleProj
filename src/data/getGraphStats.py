79b40df445a0626d9b39bab82a9d37df750d7053
gitlouis <plusel@gmail.com>
2017-01-20 18:53:45 +0200

    getGraphStats update

--- src/data/getGraphStats.py
+++ src/data/getGraphStats.py
@@ -9,7 +9,19 @@ outDegreeDistributions <LIMIT_NUM_ROWS>.pickle - dictionary of out-degrees (key 
 inDegreeDistributions <LIMIT_NUM_ROWS>.png - plot of the in-degree distribution
 inDegreeDistributions <LIMIT_NUM_ROWS>.png - plot of the out-degree distribution
 '''
+from pygraph.classes.digraph import digraph
+from collections import defaultdict, Counter
+import matplotlib.pyplot as plt
+import operator
+try:
+   import cPickle as pickle
+except:
+   import pickle
+import os
+from numpy import linspace as lrange
 
+
+print('start')
 try:
    import cPickle as pickle
 except:
@@ -17,13 +29,54 @@ except:
 import os
 
 project_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..'))
-input_dir = os.path.join(project_dir, 'reports', 'descriptive stats')
+input_dir = os.path.join(project_dir, 'data', 'raw')
 output_dir = os.path.join(project_dir, 'data', 'processed', 'sample')
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
+#Reading the files
+print("Loading the data")
+VERT_FILE_PATH = os.path.join(input_dir, 'patents.txt')
+EDGE_FILE_PATH = os.path.join(input_dir, 'citations.txt')
 
-inDegrees = pickle.load(open(os.path.join(output_dir, 'inDegreeDistributions.pickle'), 'rb'))
-outDegrees = pickle.load(open(os.path.join(output_dir, 'outDegreeDistributions.pickle'), 'rb'))
+LIMIT_NUM_ROWS = float("Inf")
+gr = digraph()
 
-inOutRatio = { k: x.get(k, 0) / y.get(k, 0) for k in set(x) | set(y) }
+print("Loading vertices")
+with open(VERT_FILE_PATH, mode='r', encoding='utf-8') as vf:
+    next(vf)
+    row_counter = 0
+    for line in vf:
+        row_counter +=1
+        gr.add_node(line.strip().split()[0])
+        if row_counter >= LIMIT_NUM_ROWS:
+            break
+
+print("reading edges")
+with open(EDGE_FILE_PATH,  mode='r', encoding='utf-8') as ef:
+    next(ef)
+    row_counter = 0
+    for edgeRow in ef:
+        if row_counter % 10000 == 0 : print(row_counter)
+        row_counter +=1
+        edge = edgeRow.split()
+        # if edge[0] == "NULL" or edge[1] == "NULL":
+        #     print(edgeRow)
+        #     print (row_counter)
+
+        if not gr.has_node(edge[0]) :gr.add_node(edge[0])
+        if not gr.has_node(edge[1]) :gr.add_node(edge[1])
+        if not gr.has_edge(tuple(edge)) : gr.add_edge(tuple(edge))
+        # outDegrees[edge[0]]+=1
+        # inDegrees[edge[1]]+=1
+        if row_counter >= LIMIT_NUM_ROWS:
+            break
+
+# inDegrees = pickle.load(open(os.path.join(output_dir, 'inDegreeDistributions.pickle'), 'rb'))
+# outDegrees = pickle.load(open(os.path.join(output_dir, 'outDegreeDistributions.pickle'), 'rb'))
+
+# inOutRatio = { k: inDegrees.get(k, 0) / outDegrees.get(k, 0) for k in set(inDegrees) | set(outDegrees) }
+
+pickle.dump(gr, open(os.path.join(output_dir, 'citationGraph '+ str(row_counter)+'.pickle'), 'wb'))
+
+print('')
