64838c8f160dd8ef85d38383a869f1d0086a2b42
gitlouis <plusel@gmail.com>
2017-01-20 19:09:52 +0200

    getGraphStats update

--- src/data/getGraphStats.py
+++ src/data/getGraphStats.py
@@ -20,17 +20,25 @@ except:
 import os
 from numpy import linspace as lrange
 
+# plot histogram of the degree distribution and save to file
+def plotDegrees(degrees, title = "degree distribution", ifShow = False, fileName = "", numOfBins = 100):
+    print( Counter(degrees).most_common(10))
+    fig = plt.figure()
+    plt.hist(list(degrees),bins=lrange(min(degrees), max(degrees), numOfBins))
+    plt.xlabel('degree')
+    plt.yscale('log')
+    plt.ylabel('log frequency')
+    plt.title(title)
+    ax = fig.add_subplot(111)
+    ax.set_xlim(0, max(degrees))
+    if ifShow : plt.show()
+    if len(fileName): plt.savefig(fileName+'.png',dpi=400)
+    plt.clf()
 
-print('start')
-try:
-   import cPickle as pickle
-except:
-   import pickle
-import os
 
 project_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..'))
 input_dir = os.path.join(project_dir, 'data', 'raw')
-output_dir = os.path.join(project_dir, 'data', 'processed', 'sample')
+output_dir = os.path.join(project_dir, 'data', 'processed')
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
@@ -71,6 +79,7 @@ with open(EDGE_FILE_PATH,  mode='r', encoding='utf-8') as ef:
         # inDegrees[edge[1]]+=1
         if row_counter >= LIMIT_NUM_ROWS:
             break
+
 
 # inDegrees = pickle.load(open(os.path.join(output_dir, 'inDegreeDistributions.pickle'), 'rb'))
 # outDegrees = pickle.load(open(os.path.join(output_dir, 'outDegreeDistributions.pickle'), 'rb'))
