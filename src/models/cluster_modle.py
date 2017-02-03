1c4c771bb5b83b236783fe388bdbaa9ce4bb4fed
motti <mottigold@gmail.com>
2017-02-03 19:35:24 +0200

    added cluster_model under src/models

---
+++ src/models/cluster_modle.py
@@ -0,0 +1,32 @@
+from sklearn.cluster import KMeans
+from src.features.ft_idfer import TfIdfer
+from scipy.sparse import hstack
+
+
+class ClusterModle:
+    """
+    perform initial clustering to the data
+    """
+
+    def __init__(self):
+        self.vectorizer = TfIdfer()
+
+    def get_labels(self, k, df, cols):
+        """
+        :param k: num of clusters
+        :param df: the dataframe to analyze
+        :param params: list contains the parameters we want to generate the clusters base on them
+        :return: list of labels - label for each row of the df
+        """
+        km = KMeans(n_clusters=k)
+        print("extract tf-idf on ", cols)
+        features_dict = self.vectorizer.get_features(df=df, cols=cols)
+        features_sparse_matrix = hstack(list(features_dict.values()))
+        print("compute "+str(k)+" clusters")
+        km.fit(features_sparse_matrix)
+        return km.labels_
+
+
+
+
+
