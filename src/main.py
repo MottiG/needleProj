ccc6547a5ce089b9828f1bb2b8470ccfb21eb11c
gitLouis <plusel@gmail.com>
2017-02-04 21:03:40 +0200

    community detection updates

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 from src.features.build_features import build_features
 from src.models.build_dendogram import build_dendogram
 from src.visualization import visualize
+from sklearn.decomposition import TruncatedSVD
 
 # Load data
 project_dir = os.path.abspath(os.path.join('__file__', '..', '..'))
@@ -12,6 +13,11 @@ df = pd.read_pickle(os.path.join(input_dir, 'patent_table_clean.pickle'))
 
 # Create features
 features_sparse_matrix = build_features(df)
+
+# feature dimension reduction
+print("feature dimension reduction")
+pca = TruncatedSVD(n_components=100)
+features_sparse_matrix = pca.fit_transform(features_sparse_matrix)
 
 # build dendogram
 dendogram = build_dendogram(features_sparse_matrix, pd.DataFrame(index=df.index.copy()))
