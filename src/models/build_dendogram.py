f30a1dd356ebad58f0c7dd7082a55eb63ba88fde
motti <mottigold@gmail.com>
2017-02-04 19:14:37 +0200

    dendogram works

--- src/models/build_dendogram.py
+++ src/models/build_dendogram.py
@@ -1,27 +1,39 @@
-"""
+from sklearn.cluster import KMeans
+import pandas as pd
 
 """
+Build a dendogram of the patents, by creating several levels of k-means clusters where each level has a
+different number of clusters
+"""
 
-from sklearn.cluster import KMeans, MiniBatchKMeans
-import time
+LEVELS = [30, 7]  # number of clusters required for each level, must be descending order.
 
 
-def build_dendogram(features_sparse_matrix):
+def build_dendogram(features_sparse_matrix, dendogram_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Build a dendogram of the patents
+    :param features_sparse_matrix: scipy csr matrix of shape (n_patents, n_features)
+    :param dendogram_df: the dataframe to populate with the results of the clustering process
+    :return: dataframe of shape (n_patents, n_levels_of_clusters) where each level has the cluster label for
+    each patent
+    """
+
+    dendogram_df = pd.concat([dendogram_df, pd.DataFrame(columns=LEVELS)], copy=False)
+
     print("K-means...")
-    for k in [7,30,100]:
+    last_centroids = None
+    for i in range(len(LEVELS)):
+        k = LEVELS[i]
         print("K-means for " + str(k) + " clusters")
-        t0 = time.time()
         km = KMeans(n_clusters=k)
-        km.fit(features)
-        km_labels = km.labels_
-        t_batch = time.time() - t0
-
-        print("MiniBatchKMeans for " + str(k) + " clusters")
-        t0 = time.time()
-        km = MiniBatchKMeans(n_clusters=k)
-        km.fit(features)
-        mbk_labels = km.labels_
-        t_mini_batch = time.time() - t0
+        if i == 0:
+            km.fit(features_sparse_matrix)
+            dendogram_df.loc[:, k] = km.labels_
+        else:
+            km.fit(last_centroids)
+            for j in range(len(last_centroids)):
+                dendogram_df.ix[dendogram_df[LEVELS[i - 1]] == j, k] = km.labels_[j]
+        last_centroids = km.cluster_centers_
     print("done.")
 
-    return dendogram
+    return dendogram_df
