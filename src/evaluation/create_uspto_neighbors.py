8009072f6fb9fe6d3c3affdb8c6b509fce05feb2
venomouse <maria.dyshel@gmail.com>
2017-02-24 14:36:02 +0200

    merge

--- src/evaluation/create_uspto_neighbors.py
+++ src/evaluation/create_uspto_neighbors.py
@@ -7,10 +7,9 @@ from collections import deque, Counter
 import matplotlib.pyplot as plt
 
 
+SAMPLE = ""
 
-SAMPLE = "sample"
-
-project_dir = os.path.abspath(os.path.join(__file__, '..', '..'))
+project_dir = os.path.abspath(os.path.join(__file__, '..', '..', '..'))
 input_dir = os.path.join(project_dir, 'data', 'processed', SAMPLE)
 figure_dir = os.path.join(project_dir, 'reports', 'figures')
 data_file = os.path.join(input_dir, 'patent_table_clean.pickle')
@@ -30,15 +29,15 @@ def select_random_pair(data_frame):
     ind1 = data_frame.index[int_ind1]
     ind2 = data_frame.index[int_ind2]
     dist = len(set(data_frame['class, mainclass'][ind1]).intersection(set(data_frame['class, mainclass'][ind2])))
-    percentage_dist = 2*float(dist) / float(len(data_frame['class, mainclass'][ind1]) + len(data_frame['class, mainclass'][ind2]))
+    percentage_dist = float(dist) / len(set(data_frame['class, mainclass'][ind1]).union(set(data_frame['class, mainclass'][ind2])))
     return ((ind1, ind2), dist, percentage_dist)
 
 
 columns = ['index1','index2', 'common class distance', 'percentage distance']
-num_connected_articles = 1000
+num_random_pairs = 1000000
+num_connected_articles = num_random_pairs/1000
 connected_articles = pd.DataFrame(index=numpy.arange(0, num_connected_articles), columns=columns)
 curr_connected_ind = 0
-num_random_pairs = 100000
 distances = deque()
 percentage_distances = deque()
 for i in range(num_random_pairs):
@@ -46,19 +45,20 @@ for i in range(num_random_pairs):
     distances.append(dist)
     percentage_distances.append(percentage_dist)
     if (dist > 0):
-        connected_articles.loc[curr_connected_ind] = [ind1, ind2, dist, percentage_dist]
-        curr_connected_ind+=1
+        if curr_connected_ind < num_connected_articles:
+            connected_articles.loc[curr_connected_ind] = [ind1, ind2, dist, percentage_dist]
+            curr_connected_ind+=1
 
 cnt = Counter(list(distances))
 fig, ax = plt.subplots()
 ax.bar(list(cnt.keys()), list(cnt.values()))
 ax.set_yscale('log')
-plt.savefig('distance_statistics.png')
+plt.savefig(os.path.join(figure_dir, 'distance_statistics.png'))
 
 fig, ax2 = plt.subplots()
-n, bins, patches = plt.hist(list(percentage_distances), 5)
+n, bins, patches = plt.hist(list(percentage_distances), 10)
 ax2.set_yscale('log')
-plt.savefig('percentage_distance_statistic.png')
+plt.savefig(os.path.join(figure_dir, 'percentage_distance_statistic.png'))
 
 connected_articles = connected_articles[:curr_connected_ind+1]
 pd.to_pickle(connected_articles, connected_articles_file)
