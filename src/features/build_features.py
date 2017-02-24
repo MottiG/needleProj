80ac7f92de67882baf53b588bd1f965f39a21891
motti <mottigold@gmail.com>
2017-02-24 21:35:40 +0200

    change some flow. now we pca only the tfidf

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,33 +1,39 @@
-import os
-from scipy.sparse import hstack
+from scipy import sparse
+import numpy as np
 from .ft_idfer import TfIdfer
-from .graph_features_builder import GraphFeaturesBuilder
+from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import Normalizer
+from sklearn.pipeline import make_pipeline
+# from .graph_features_builder import GraphFeaturesBuilder
 
 """
 build all the features of the patents - text features (tfidf vectors) and graph features (communities)
 """
 
-COLS = ['title', 'abstract']  # name of columns to apply tfidf vectorization
-N_COMMUNITIES_LIST = [7, 100, 1000]  # number of communities to find in the graph of patents
-
-
-def build_features(df):
+def build_features(df, cols_of_tfidf, n_components):
     """
     build all the features of the patents
     :param df: pandas DataFrame contains the patents data
+    :param cols_of_tfidf: list of columns names to get tf-idf vectors for this columns
+    :param n_components: number of components to save after dimension reducing of tfidf matrices
     :return: scipy csr matrix of shape (n_patents, n_features)
     """
 
-    # project_dir = os.path.abspath(os.path.join( '__file__' , '..', '..','..'))
-    # input_dir = os.path.join(project_dir, 'data', 'processed', 'sample')
-
+    print('Getting tfidf features')
     tfIdfer = TfIdfer()
-    tf_idf_features_dict = tfIdfer.get_features(df, COLS)
-    print('Got tfidt features')
+    tf_idf_features_dict = tfIdfer.get_features(df, cols_of_tfidf)
 
+    # feature dimension reduction
+    print("feature dimension reduction")
+    tfifd_features_sprs_matrix = sparse.hstack(list(tf_idf_features_dict.values()))
+    del tf_idf_features_dict  # TODO check that its not reference!
+    lsa = make_pipeline(TruncatedSVD(n_components), Normalizer(copy=False))
+    tfifd_features_matrix = lsa.fit_transform(tfifd_features_sprs_matrix)
+
+    #print('Getting graph features')
     # graph_geatures_Builder = GraphFeaturesBuilder()
-    # graph_features_dict = graph_geatures_Builder.get_features(N_COMMUNITIES_LIST)
+    # graph_features = graph_geatures_Builder.get_features(N_COMMUNITIES_LIST)
     # print('Got graph features')
 
-    features_sparse_matrix = hstack(list(tf_idf_features_dict.values()))
-    return features_sparse_matrix
+    features_matrix = tfifd_features_matrix # np.hstack(list(tfifd_features_matrix))
+    return features_matrix
